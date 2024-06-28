8d60fa578d9a47f209a64f7fced5527ef4790673
JakePaul11 <mumukuku346@gmail.com>
2024-06-28 22:40:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let ism = prompt("Ismingizni kiriting")
+let id = +prompt("ID kiriting")
+let parol = +prompt("Parolingizni kiriting")
+
+const person = {
+    Name: ism,
+    ID: id,
+    Password: parol
+  }; 
+
+  console.log(person)
